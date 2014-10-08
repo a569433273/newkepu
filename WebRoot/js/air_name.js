35662680a80c0b41a8992c6430552d46aab828cd
a569433273 <569433273@qq.com>
2014-10-08 16:25:09 +0800

    查询预定功能做完，注册和常旅客的基本方法写好

--- WebRoot/js/air_name.js
+++ WebRoot/js/air_name.js
@@ -1,37 +1,103 @@
 // 对应航班
-function air_n(Carrier){
-                if(Carrier=='CA'){air_name='中国国际航空'}
-				else if(Carrier=='CZ'){air_name='中国南方航空'}
-				else if(Carrier=='MU'){air_name='中国东方航空'}
-				else if(Carrier=='HU'){air_name='海南航空'}
-				else if(Carrier=='3U'){air_name='四川航空'}
-				else if(Carrier=='SC'){air_name='山东航空'}
-				else if(Carrier=='CN'){air_name='大新华航空'}
-				else if(Carrier=='JD'){air_name='金鹿航空'}
-				else if(Carrier=='GS'){air_name='天津航空'}
-				else if(Carrier=='KN'){air_name='中国联合航空'}
-				else if(Carrier=='ZH'){air_name='深圳航空'}
-				else if(Carrier=='MF'){air_name='厦门航空'}
-				else if(Carrier=='8C'){air_name='东星航空'}
-				else if(Carrier=='8L'){air_name='祥鹏航空'}
-				else if(Carrier=='9C'){air_name='春秋航空'}
-				else if(Carrier=='AK'){air_name='亚洲航空'}
-				else if(Carrier=='BK'){air_name='奥凯航空'}
-				else if(Carrier=='BR'){air_name='长荣航空'}
-				else if(Carrier=='CI'){air_name='中华航空'}
-				else if(Carrier=='EU'){air_name='成都航空'}
-				else if(Carrier=='FM'){air_name='上海航空'}
-				else if(Carrier=='HO'){air_name='吉祥航空'}
-				else if(Carrier=='KA'){air_name='港龙航空'}
-				else if(Carrier=='KY'){air_name='昆明航空'}
-				else if(Carrier=='NX'){air_name='澳门航空'}
-				else if(Carrier=='NS'){air_name='东北航空'}
-				else if(Carrier=='PN'){air_name='西部航空'}
-				else if(Carrier=='SZ'){air_name='中国西南航空'}
-				else if(Carrier=='VD'){air_name='河南航空公司'}
-				else if(Carrier=='WH'){air_name='中国西北航空'}
-				else if(Carrier=='WU'){air_name='武汉航空'}
-				else  if(Carrier=='XO'){air_name='新疆航空'}
-				else  if(Carrier=='Z2'){air_name='中原航空'}
-				return air_name;
+function air_n(Carrier) {
+    if (Carrier == 'CA') {
+        air_name = '中国国际航空'
+    }
+    else if (Carrier == 'CZ') {
+        air_name = '中国南方航空'
+    }
+    else if (Carrier == 'MU') {
+        air_name = '中国东方航空'
+    }
+    else if (Carrier == 'HU') {
+        air_name = '海南航空'
+    }
+    else if (Carrier == '3U') {
+        air_name = '四川航空'
+    }
+    else if (Carrier == 'SC') {
+        air_name = '山东航空'
+    }
+    else if (Carrier == 'CN') {
+        air_name = '大新华航空'
+    }
+    else if (Carrier == 'JD') {
+        air_name = '金鹿航空'
+    }
+    else if (Carrier == 'GS') {
+        air_name = '天津航空'
+    }
+    else if (Carrier == 'KN') {
+        air_name = '中国联合航空'
+    }
+    else if (Carrier == 'ZH') {
+        air_name = '深圳航空'
+    }
+    else if (Carrier == 'MF') {
+        air_name = '厦门航空'
+    }
+    else if (Carrier == '8C') {
+        air_name = '东星航空'
+    }
+    else if (Carrier == '8L') {
+        air_name = '祥鹏航空'
+    }
+    else if (Carrier == '9C') {
+        air_name = '春秋航空'
+    }
+    else if (Carrier == 'AK') {
+        air_name = '亚洲航空'
+    }
+    else if (Carrier == 'BK') {
+        air_name = '奥凯航空'
+    }
+    else if (Carrier == 'BR') {
+        air_name = '长荣航空'
+    }
+    else if (Carrier == 'CI') {
+        air_name = '中华航空'
+    }
+    else if (Carrier == 'EU') {
+        air_name = '成都航空'
+    }
+    else if (Carrier == 'FM') {
+        air_name = '上海航空'
+    }
+    else if (Carrier == 'HO') {
+        air_name = '吉祥航空'
+    }
+    else if (Carrier == 'KA') {
+        air_name = '港龙航空'
+    }
+    else if (Carrier == 'KY') {
+        air_name = '昆明航空'
+    }
+    else if (Carrier == 'NX') {
+        air_name = '澳门航空'
+    }
+    else if (Carrier == 'NS') {
+        air_name = '东北航空'
+    }
+    else if (Carrier == 'PN') {
+        air_name = '西部航空'
+    }
+    else if (Carrier == 'SZ') {
+        air_name = '中国西南航空'
+    }
+    else if (Carrier == 'VD') {
+        air_name = '河南航空公司'
+    }
+    else if (Carrier == 'WH') {
+        air_name = '中国西北航空'
+    }
+    else if (Carrier == 'WU') {
+        air_name = '武汉航空'
+    }
+    else if (Carrier == 'XO') {
+        air_name = '新疆航空'
+    }
+    else if (Carrier == 'Z2') {
+        air_name = '中原航空'
+    }
+    return air_name;
 }
